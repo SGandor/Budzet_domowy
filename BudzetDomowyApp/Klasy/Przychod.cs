2f09ff974061d145b4f74964b30df7b692ce4196
SGandor <gandor.seweryn@gmail.com>
2025-02-22 16:26:18 +0100

    Klasy

---
+++ BudzetDomowyApp/Klasy/Przychod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Wydatek : Transakcja
+{
+    public Wydatek(DateTime data, decimal kwota, string opis, string kategoria)
+    {
+        Data = data;
+        Kwota = kwota;
+        Opis = opis;
+        Kategoria = kategoria;
+        Typ = "Wydatek";
+    }
+}
