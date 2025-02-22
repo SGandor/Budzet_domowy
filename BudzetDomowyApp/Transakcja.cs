16f9d6ec9f96673f6b9fb8de88e11fc591005bba
SGandor <gandor.seweryn@gmail.com>
2025-02-23 00:04:22 +0100

    Klasy transakcji

---
+++ BudzetDomowyApp/Transakcja.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudzetDomowyApp
+{
+    public class Transakcja
+    {
+        public DateTime Data { get; set; }
+        public decimal Kwota { get; set; }
+        public string Nazwa { get; set; }
+        public string Opis { get; set; }
+        public string Kategoria { get; set; }
+        public string Typ { get; set; }  // "Przychód" lub "Wydatek"
+    }
+}
