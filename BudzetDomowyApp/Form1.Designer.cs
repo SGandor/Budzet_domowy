cdfcf5c5cff2957a5625ccbdd1026c40b75c3eb6
SGandor <gandor.seweryn@gmail.com>
2025-02-23 00:40:43 +0100

    Test eksportu do Excela

--- BudzetDomowyApp/Form1.Designer.cs
+++ BudzetDomowyApp/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnEksport = new Button();
+            SuspendLayout();
+            // 
+            // btnEksport
+            // 
+            btnEksport.Location = new Point(551, 361);
+            btnEksport.Name = "btnEksport";
+            btnEksport.Size = new Size(237, 77);
+            btnEksport.TabIndex = 0;
+            btnEksport.Text = "Eksportuj do Excel";
+            btnEksport.UseVisualStyleBackColor = true;
+            btnEksport.Click += btnEksport_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnEksport);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
         }
 
         #endregion
+
+
+        private Button btnEksport;
     }
 }
--- BudzetDomowyApp/Form1.cs
+++ BudzetDomowyApp/Form1.cs
@@ -6,5 +6,32 @@ namespace BudzetDomowyApp
         {
             InitializeComponent();
         }
+
+        private Budzet budzet = new Budzet();
+
+        private void btnEksport_Click(object sender, EventArgs e)
+        {
+            budzet.DodajTransakcje(new Przychod
+            {
+                Data = DateTime.Now,
+                Kwota = 5000,
+                Nazwa = "Wyp³ata",
+                Opis = "Miesiêczna wyp³ata",
+                Kategoria = "Praca"
+            });
+
+            budzet.DodajTransakcje(new Wydatek
+            {
+                Data = DateTime.Now,
+                Kwota = 200,
+                Nazwa = "Zakupy",
+                Opis = "Codzienne zakupy",
+                Kategoria = "Jedzenie"
+            });
+
+            ExcelTabela.EksportujDoExcel(budzet.Transakcje, "budzet.xlsx");
+            MessageBox.Show("Dane zosta³y wyeksportowane do pliku 'budzet.xlsx'!");
+        }
+
     }
 }
